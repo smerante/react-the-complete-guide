d628a3922e23fdc87eef093b9af58cf13d66d909
Sam <merante.s@hotmail.com>
2022-01-30 12:07:09 -0500

    building and reusing a component

--- section-29-react-summary/src/App.tsx
+++ section-29-react-summary/src/App.tsx
@@ -1,24 +1,12 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.scss';
+import { Todo } from './components/Todo';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>My Todos</h1>
+      <Todo/>
     </div>
   )
 }
---
+++ section-29-react-summary/src/components/Todo.tsx
@@ -0,0 +1,10 @@
+export const Todo = () => {
+    return (
+        <div className='card'>
+            <h2>Title</h2>
+            <div className='action'>
+                <button className='btn'>Delete</button>
+            </div>
+        </div>
+    )
+};
