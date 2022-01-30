6e1b003f33a8d9ecb6ad5e4a4698d64001dd0546
smerante <merante.s@hotmail.com>
2022-01-30 18:10:43 -0500

    use effect to fetch data from firebase

--- section-29-react-summary/src/pages/AllMeetups.tsx
+++ section-29-react-summary/src/pages/AllMeetups.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const MOCK_DATA = [
@@ -21,10 +22,42 @@ const MOCK_DATA = [
     },
 ]
 const AllMeetupsPage = () => {
+
+    const [isLoading, setLoading] = useState(true);
+    const [meetupData, setData] = useState([]);
+
+    useEffect(() => {
+        console.warn("Fetch meetups");
+        fetch('https://react-getting-started-48366-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'GET',
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            }
+        ).then((res) => {
+            res.json().then(data => {
+                setLoading(false);
+                const dataArr: any = [];
+                for(const key in data){
+                    dataArr.push({
+                        id: key,
+                        ...data[key]
+                    });
+                }
+                setData(dataArr);
+            })
+        }).catch(e => {
+            console.error('error: ', e);
+        });
+    }, []);
+
     return <section>
         <h1>All Meetups</h1>
+        {
+            isLoading ? <p>Loading...</p> : <MeetupList data={meetupData} />
+        }
 
-       <MeetupList data={MOCK_DATA}/>
     </section>
 };
 
